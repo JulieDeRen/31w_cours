def444a63f2a7aac8fd5ea03e5be4a5fe49bf6de
JulieDeRen <julie.deschenes-renaud@hec.ca>
2022-10-19 14:52:12 -0400

    m

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Underscore
+ */
+
+?>
+
+	<footer class="site__footer">
+		<h2>Pied de page du 31w</h2>
+	</footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?><!--générer javascript-->
+
+</body>
+</html>
