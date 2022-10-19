1f1c697244f3b7ac6c4216f3d3d8eeb42a4be9db
JulieDeRen <julie.deschenes-renaud@hec.ca>
2022-10-19 13:57:03 -0400

    Ajout de fonction menu

--- header.php
+++ header.php
@@ -26,6 +26,10 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'igc31w' ); ?></a>
 
 	<header id="masthead" class="site__header"> <!--modifié site-header standard BEM -->
+		
+		<?php
+			wp_nav_menu(()=>{})
+		?>
 		<div class="site__branding"><!--modifié site-branding standard BEM-->
 			<?php
 			the_custom_logo();
