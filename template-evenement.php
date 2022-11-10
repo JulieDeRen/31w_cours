df36b017a9c5fd4477656735506749b317025365
JulieDeRen <julie.deschenes-renaud@hec.ca>
2022-11-10 16:31:10 -0500

    c13-ajout de la page template-evenement.php

--- template-evenement.php
+++ template-evenement.php
@@ -11,8 +11,8 @@
      <?php if (have_posts()): while(have_posts()): the_post(); ?>
      <?php the_title() ?>
      <?php the_content() ?>
-     <p><?php the_field('adresse') ?></p>
-     <p><?php the_field('date_et_heure_de_levenement') ?></p>
+     <p>L'événement aura lieu au :  <?php the_field('adresse') ?></p>
+     <p>La date et l'heure de l'énénement :  <?php the_field('date_et_heure_de_levenement') ?></p>
      <?php endwhile ?>
      <?php endif ?>
 </main>
