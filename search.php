af533a3af77f4033f94863f55b768945b20ba62b
JulieDeRen <julie.deschenes-renaud@hec.ca>
2022-12-13 17:42:18 -0500

    modification de la page recherche avec mise en page

--- search.php
+++ search.php
@@ -18,31 +18,40 @@ get_header();
 <h1>search.php</h1>
 	<main class="site__main">
 
-		<?php
+    <?php 
 
 
             while ( have_posts() ) : the_post();
-            $i=0; /*compteur */
-            $i++; ?>
-            <h3> Nous avons trouvé <?= $i ?> résultat(s) de recherche </h3>
+
+            $i=0; /*compteur */   
+             ?>
+
+
 
             <?php
             if ( have_posts() ) :
 			/* Start the Loop */
 				the_post(); ?>
+            
+            <?php $i++; /*compteur */?>
+            <?php $message = "Nous avons trouvé " . $i . " résultat(s) de recherche"; ?>
+            <h3> <?= $message ?> </h3>
+
 
             <div class= "widget-rechercher">
                 <?php the_post_thumbnail('medium'); ?>
                 <div class="widget-rechercher-texte">
                     <h1><?= get_the_title(); ?></h1>
-                    <p>L'événement aura lieu au :  <?php the_field('adresse') ?></p>
-                    <p>La date et l'heure de l'énénement :  <?php the_field('date_et_heure_de_levenement') ?></p>
-                    <?php the_content();?>
+                    <p>L'événement aura lieu au :  <?php the_field('adresse');  ?></p>
+                    <p>La date et l'heure de l'énénement :  <?php the_field('date_et_heure_de_levenement'); ?></p>
+                    <p><?= wp_trim_words(get_the_excerpt(), 20,$le_permalien) ; ?></p>
                 </div>
             </div>
 
             <?php endif; ?>	
             <?php
+
+
                 endwhile;
             ?>
 
